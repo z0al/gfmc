5a2a24abb21c53e2eb749270cdd85c13af25f58a
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-19 11:43:07 +0300

    refactor: render `<hr>` as open tag

--- src/render.ts
+++ src/render.ts
@@ -3,6 +3,7 @@
  */
 export interface Renderer {
   atx_heading(text: string, level: number): string
+  setext_heading(text: string, level: number): string
   thematic_break(char: string): string
 }
 
@@ -13,7 +14,10 @@ export class HTMLRenderer implements Renderer {
   public atx_heading(text: string, level: number) {
     return `<h${level}>${text}</h${level}>`
   }
+  public setext_heading(text: string, level: number) {
+    return this.atx_heading(text, level)
+  }
   public thematic_break(char: string) {
-    return '<hr/>'
+    return '<hr>'
   }
 }
