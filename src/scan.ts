8612524aa17149e491a72b8d4d08133a41b08d35
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-20 14:26:25 +0300

    doc: some docs

--- src/scan.ts
+++ src/scan.ts
@@ -1,48 +1,5 @@
 import * as t from './tokens'
 
-// We can think of a document as a sequence of blocks—structural elements like
-// paragraphs, block quotations, lists, headings, rules, and code blocks. Some
-// blocks (like block quotes and list items) contain other blocks; others (like
-// headings and paragraphs) contain inline content—text, links, emphasized
-// text, images, code spans, and so on.
-//
-// ref: https://github.github.com/gfm/#blocks-and-inlines
-const rules = {
-  // ========================================================================
-  // >     LEAF BLOCKS                                                      <
-  // ========================================================================
-
-  // An ATX heading consists of a string of characters, parsed as inline
-  // content, between an opening sequence of 1–6 unescaped # characters and an
-  // optional closing sequence of any number of unescaped # characters. The
-  // opening sequence of # characters must be followed by a space or by the end
-  // of line. The optional closing sequence of #s must be preceded by a space
-  // and may be followed by spaces only. The opening # character may be
-  // indented 0-3 spaces.
-  //
-  // ref: https://github.github.com/gfm/#atx-heading
-  atx_heading: /^(?: {0,3})([#]{1,6})(?:(?:(?: (.*))?( #{0,}))|(?: (.*))?)(?=\n|$)/,
-
-  // A setext heading consists of one or more lines of text, each containing at
-  // least one non-whitespace character, with no more than 3 spaces indentation
-  // followed by a setext heading underline. The lines of text must be such
-  // that were they not followed by the setext heading underline, they would be
-  // interpreted as a paragraph: they cannot be interpretable as a code fence,
-  // ATX heading, block quote, thematic break, list item, or HTML block.
-  //
-  // ref: https://github.github.com/gfm/#setext-heading
-  setext_heading: /^((?:(?: {0,3})(?:(?![\*\-\+] |\d+(?:\)|\.) |\s)[^\n]*)\n)+)(?: {0,3})(-{2,}|=+) *(?=\n|$)/,
-
-  // A line consisting of 0-3 spaces of indentation, followed by a sequence of
-  // three or more matching -, _, or * characters, each followed optionally by
-  // any number of spaces. It is required that all of the non-whitespace
-  // characters be the same. Spaces are allowed at the end. However, no other
-  // characters may occur in the line
-  //
-  // ref: https://github.github.com/gfm/#thematic-breaks
-  thematic_break: /^(?: {0,3})([-*_]) *(?:\1 *){2,}(?=\n|$)/
-}
-
 // Helpers
 const indent = /^( {4,})\S/
 const spaces = /^ */
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,14 +1,36 @@
-// Token interface
+// General Token interface
 export interface Token {
   type: string
 }
 
-// Thematic Break token
+// A line consisting of 0-3 spaces of indentation, followed by a sequence of
+// three or more matching -, _, or * characters, each followed optionally by
+// any number of spaces. It is required that all of the non-whitespace
+// characters be the same. Spaces are allowed at the end. However, no other
+// characters may occur in the line
+//
+// ref: https://github.github.com/gfm/#thematic-breaks
 export interface ThematicBreak extends Token {
   char: string
 }
 
-// Heading token
+// An ATX heading consists of a string of characters, parsed as inline
+// content, between an opening sequence of 1–6 unescaped # characters and an
+// optional closing sequence of any number of unescaped # characters. The
+// opening sequence of # characters must be followed by a space or by the end
+// of line. The optional closing sequence of #s must be preceded by a space
+// and may be followed by spaces only. The opening # character may be
+// indented 0-3 spaces.
+//
+// A setext heading consists of one or more lines of text, each containing at
+// least one non-whitespace character, with no more than 3 spaces indentation
+// followed by a setext heading underline. The lines of text must be such
+// that were they not followed by the setext heading underline, they would be
+// interpreted as a paragraph: they cannot be interpretable as a code fence,
+// ATX heading, block quote, thematic break, list item, or HTML block.
+//
+// ref: https://github.github.com/gfm/#atx-heading
+// ref: https://github.github.com/gfm/#setext-heading
 export interface Heading extends Token {
   text: string
   level: number
