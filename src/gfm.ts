40807946f669f5c242e566cf4d5308529b55d6b8
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-21 19:58:04 +0300

    chore: rename scan.ts to blocks.ts

--- src/gfm.ts
+++ src/gfm.ts
@@ -1,8 +1,8 @@
 // Ours
+import { Scanner } from './blocks'
 import { Options } from './options'
 import { Parser } from './parse'
 import { HTMLRenderer } from './render'
-import { Scanner } from './scan'
 
 export default (src: string, options: Options = {}): string => {
   const scanner = new Scanner(src)
