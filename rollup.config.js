f81e080c5e5e83d46b0780ec9ce8a3eb3e308124
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-10-21 11:22:26 +0300

    build: generate rollup test entries dynamically

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,46 @@
+// Native
+import { readdirSync } from 'fs'
+import { resolve, basename } from 'path'
+
 // Packages
 import typescript from 'rollup-plugin-typescript'
 
 // Ours
 import pkg from './package.json'
 
+const plugins = [
+  typescript({
+    typescript: require('typescript')
+  })
+]
+
+// Generates Rollup entries for test files
+const testFiles = () => {
+  const entries = readdirSync(resolve(__dirname, 'test'))
+  return entries.map(e => {
+    return {
+      input: `test/${e}`,
+      output: [{ file: `dist/${basename(e, '.ts')}-test.js`, format: 'cjs' }],
+      external: ['ava'],
+      plugins
+    }
+  })
+}
+
 export default [
+  // Main bundle
   {
     input: 'src/index.ts',
     output: [
-      {
-        // CommonJS (for Node) build
-        file: pkg.main,
-        format: 'cjs'
-      },
-      {
-        // browser-friendly UMD build
-        file: pkg.browser,
-        format: 'umd',
-        name: 'gfmc'
-      },
-      {
-        // ES module (for bundlers) build
-        file: pkg.module,
-        format: 'es'
-      }
+      // CommonJS (for Node) build
+      { file: pkg.main, format: 'cjs' },
+      // Browser-friendly UMD build
+      { file: pkg.browser, format: 'umd', name: 'gfmc' },
+      // ES module (for bundlers) build
+      { file: pkg.module, format: 'es' }
     ],
-    plugins: [
-      typescript({
-        typescript: require('typescript')
-      })
-    ]
+    plugins
   },
-  {
-    // Test script
-    input: 'test/spec.ts',
-    output: [
-      {
-        file: 'dist/spec-test.js',
-        format: 'cjs'
-      }
-    ],
-    external: ['ava', 'html-minifier'],
-    plugins: [
-      typescript({
-        typescript: require('typescript')
-      })
-    ]
-  }
+  // Test files
+  ...testFiles()
 ]
